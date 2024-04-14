c8d04aad3d141e41aca36e4a1343c5e42f4af37a
Jonathan Ferreira <jhouus33@gmail.com>
2024-04-14 15:12:54 -0300

    add aquivos e dependencias iniciais

---
+++ ExpressMail/src/main/java/ExpressMail/ms/ExpressMailMsApplication.java
@@ -0,0 +1,13 @@
+package ExpressMail.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressMailMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressMailMsApplication.class, args);
+	}
+
+}
---
+++ ExpressMail/src/test/java/ExpressMail/ms/ExpressMailMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ExpressMail.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressMailMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
